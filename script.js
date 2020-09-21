ecd45f89223902d0736ebfdd2da8e73928bc7507
Giuseppe <giuseppedf8711@gmail.com>
2020-09-21 16:45:56 +0200

    inizio da capo

--- script.js
+++ script.js
@@ -1,75 +1,26 @@
 $(document).ready (function() {
-  // data iniziale del caledario
-  var date = "2018-01-01";
 
-  var momentDate = moment(date);
+  for (var i = 1; i <= 31; i++) {
+    var source = $("#day-template").html();
+    var template = Handlebars.compile(source);
 
-  // teamplate dei giorni del mio calendario da prendere su handlebars
-  // cambiando la source
-  var source = $("#day-template").html();
-  var template = Handlebars.compile(source);
-
-
-    // stampo i giorni del mio calendario
-    for (var i = 1; i <=momentDate.daysInMonth(); i++) {
-
-      var day = addZero(i);
-
-      var dateComplete = momentDate.format("YYYY") + "-" + momentDate.format("MM") + "-" + day;
-
-      var context = {
-        "day": i,
-        "month": momentDate.format("MMMM"),
-        "dateComplete": dateComplete
-      };
-
-      var html = template(context);
-
-      $("#days").append(html);
+    var data = {
+      "day": i,
+      "month": "Gennaio"
+    };
 
 
-    }
+    var html = template(data);
 
-    $.ajax(
-      {
-        "url": "https://flynn.boolean.careers/exercises/api/holidays",
-        "data": {
-          "year": 2018,
-          "month": 0
-        },
-        "method": "GET",
-        "success": function(data) {
-          printHolidays(data.response);
+    // andiamo ad appendere in lista days il tamplate(html) che ci siamo
+    // preparati prima
+    $("#list-days").append(html);
+  }
 
-        },
 
-        "error": function(err) {
-          alert("Errore");
-        }
-      }
-    )
+
+
+
+
+
 });
-
-function printHolidays(holidays) {
-
-  if(holidays.lenght > 0) {
-    for (var i = 0; i < holidays.length; i++) {
-
-      var holidayDate = holidays[i].date;
-      var holidayName = holidays[i].name;
-
-      $(".day[data-date='"+ holidayDate+"']").addClass("holiday");
-      $(".day[data-date='"+ holidayDate+"'] .holidayType").text(" - " + holidayName);
-
-    }
-  }
-}
-
-
-
-function addZero(day) {
-  if(day < 10) {
-    return "0" + day;
-  }
-   return  day;
-}
