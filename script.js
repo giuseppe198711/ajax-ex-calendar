baad9ea9b7b2ac58c7c11cf340c1f6c50f9c743d
Giuseppe <giuseppedf8711@gmail.com>
2020-09-21 09:30:42 +0200

    primo step

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready (function() {
   // data iniziale del caledario
-  var data = "2018-01-01";
+  var date = "2018-01-01";
 
   var momentDate = moment(date);
 
@@ -10,26 +10,66 @@ $(document).ready (function() {
   var template = Handlebars.compile(source);
 
 
-  // stampo i giorni del mio calendario
-  for (var i = 1; i <=momentDate.daysInMonth(); i++) {
+    // stampo i giorni del mio calendario
+    for (var i = 1; i <=momentDate.daysInMonth(); i++) {
+
+      var day = addZero(i);
+
+      var dateComplete = momentDate.format("YYYY") + "-" + momentDate.format("MM") + "-" + day;
+
+      var context = {
+        "day": i,
+        "month": momentDate.format("MMMM"),
+        "dateComplete": dateComplete
+      };
+
+      var html = template(context);
+
+      $("#days").append(html);
 
 
-    var context = {
-      "day": i,
-      "month": momentDate.format("MMMM")
-    };
+    }
 
-    var html = teamplate(context);
+    $.ajax(
+      {
+        "url": "https://flynn.boolean.careers/exercises/api/holidays",
+        "data": {
+          "year": 2018,
+          "month": 0
+        },
+        "method": "GET",
+        "success": function(data) {
+          printHolidays(data.response);
 
-    $("#days").append(html)
+        },
 
-
-
-
-  }
-
-
-
-
-
+        "error": function(err) {
+          alert("Errore");
+        }
+      }
+    )
 });
+
+function printHolidays(holidays) {
+
+  if(holidays.lenght > 0) {
+    for (var i = 0; i < holidays.length; i++) {
+
+      var holidayDate = holidays[i].date;
+      var holidayName = holidays[i].name;
+
+      $(".day[data-date='"+ holidayDate+"']").addClass("holiday");
+      $(".day[data-date='"+ holidayDate+"'] .holidayType").text(" - " + holidayName);
+
+    }
+  }
+}
+
+
+
+function addZero(day) {
+  if(day < 10) {
+    return "0" + day;
+  }
+   return  day;
+}
